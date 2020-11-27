de56ec6c74ba14228bf593ea79d2b3321f957f88
ssw03270 <ttd8591@gmail.com>
2020-11-27 13:45:40 +0900

    [Add] - Add activity_job.xml and need to make 'try page'

--- app/src/main/java/com/example/khuinkhujik/JobActivity.java
+++ app/src/main/java/com/example/khuinkhujik/JobActivity.java
@@ -3,8 +3,12 @@ package com.example.khuinkhujik;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Debug;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -14,6 +18,7 @@ import java.nio.charset.Charset;
 
 public class JobActivity extends AppCompatActivity {
 
+    TextView textTitle, textPay, textDay, textTime, textWork, textCondition;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,12 +31,36 @@ public class JobActivity extends AppCompatActivity {
         String jobTime = intent.getExtras().getString("jobTime");
         String jobWork = intent.getExtras().getString("jobWork");
         String jobCondition = intent.getExtras().getString("jobCondition");
-        String jobURL = intent.getExtras().getString("jobURL");
 
 
-        Log.d("log01", jobTitle + jobPay + jobDay + jobTime + jobWork + jobCondition + jobURL);
+        textTitle = findViewById(R.id.textTitle);
+        textPay = findViewById(R.id.textPay);
+        textDay = findViewById(R.id.textDay);
+        textTime = findViewById(R.id.textTime);
+        textWork = findViewById(R.id.textWork);
+        textCondition = findViewById(R.id.textCondition);
+
+        textTitle.setText(jobTitle);
+        textPay.setText("임금 : " + jobPay);
+        textDay.setText("근무형태 : " + jobDay);
+        textTime.setText("근무시간 : " + jobTime);
+        textWork.setText("직무내용 : " + jobWork);
+        textCondition.setText("경력조건 : " + jobCondition);
+
 
 
     }
 
+    public void infoButton(View v){
+        Intent intentUrl = getIntent();
+        String jobURL = intentUrl.getExtras().getString("jobURL");
+
+        Log.d("log01", jobURL);
+        Uri uri = Uri.parse(jobURL); // missing 'http://' will cause crashed
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+    public void tryButton(View v){
+        Log.d("log01", "aaaa");
+    }
 }
