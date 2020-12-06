7e54d1856d88b6bd5f1a9ba0856d780cff78b355
Park <park102001@gmail.com>
2020-12-06 21:47:55 +0900

    [Add] - Modify App Imformation Activity ( FunctionInformationActivity )

--- app/src/main/java/com/example/khuinkhujik/FunctionInformationActivity.java
+++ app/src/main/java/com/example/khuinkhujik/FunctionInformationActivity.java
@@ -1,9 +1,12 @@
 package com.example.khuinkhujik;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,35 +16,59 @@ import androidx.viewpager.widget.ViewPager;
 public class FunctionInformationActivity extends AppCompatActivity {
 
     private ViewPager mSlideViewPager;
+    private ImageView prevImageView;
+    private ImageView nextImageView;
     private TextView textView;
     private InformationSlideAdapter informationSlideAdapter;
 
     boolean canNextPage = false;
+
+    int mCurrentPage;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_function_information);
 
+        textView = (TextView)findViewById(R.id.nextPageTextView);
+
         mSlideViewPager = (ViewPager)findViewById(R.id.slideViewPager);
-        textView = (TextView)findViewById(R.id.scrollInfoTextView);
+
+        prevImageView = (ImageView)findViewById(R.id.prevImageView);
+        nextImageView = (ImageView)findViewById(R.id.nextImageView);
 
         informationSlideAdapter = new InformationSlideAdapter(this);
 
         mSlideViewPager.setAdapter(informationSlideAdapter);
         mSlideViewPager.addOnPageChangeListener(viewListener);
 
-        textView.setOnClickListener(new View.OnClickListener(){
+        prevImageView.setOnClickListener(new View.OnClickListener(){
             public void onClick(View view){
-                if(canNextPage){
+                if(mCurrentPage > 0) mSlideViewPager.setCurrentItem(mCurrentPage - 1);
+            }
+        });
+        nextImageView.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                if(mCurrentPage < 6) mSlideViewPager.setCurrentItem(mCurrentPage + 1);
+            }
+        });
+    }
+
+    public boolean onTouchEvent(MotionEvent event){
+        switch (event.getAction()){
+            case MotionEvent.ACTION_UP:
+                if(mCurrentPage == 6){
                     Intent intent = new Intent(getApplicationContext(),SelectorActivity.class);
                     startActivity(intent);
                     overridePendingTransition(0, 0);
                     FunctionInformationActivity.this.finish();
-                }else{}
-            }
-        });
+                }
+            default:
+                break;
+        }
+        return true;
     }
+
     ViewPager.OnPageChangeListener viewListener = new ViewPager.OnPageChangeListener() {
         @Override
         public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
@@ -50,12 +77,22 @@ public class FunctionInformationActivity extends AppCompatActivity {
 
         @Override
         public void onPageSelected(int position) {
-            if(position==6){
-                textView.setText("여기를 터치하시면\n앱을 시작할 수 있습니다");
-                canNextPage=true;
+
+            mCurrentPage = position;
+
+            if(position == 6){
+                nextImageView.setVisibility(View.INVISIBLE);
+                prevImageView.setVisibility(View.VISIBLE);
+                textView.setText("시작하려면 여기를 누르세요");
+            }else if(position == 0){
+                prevImageView.setVisibility(View.INVISIBLE);
+                nextImageView.setVisibility(View.VISIBLE);
+                textView.setText("");
+                canNextPage = false;
             }else{
-                textView.setText("다음으로 넘어가려면\n오른쪽으로 넘기세요"); //
-                canNextPage=false;
+                nextImageView.setVisibility(View.VISIBLE);
+                prevImageView.setVisibility(View.VISIBLE);
+                textView.setText("");
             }
         }
 
@@ -64,4 +101,6 @@ public class FunctionInformationActivity extends AppCompatActivity {
 
         }
     };
+
+
 }
